9d482aab4ee34fd0b599c7e5118470b0101ed098
jw-zhen <C108156105@nkust.edu.tw>
2021-04-15 11:13:27 +0800

    0415

---
+++ 0401react_hw/src/A1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {color:'red'};
+    }
+    render(){
+        return <h2>I am a {this.state.color} {this.props.brand}</h2>}
+}
+App.defaultProps={
+    color:'blue'
+};
+
+export default App;
