c525a9d303b7105d5d3fd8f767b3dce51fe19481
jw-zhen <C108156105@nkust.edu.tw>
2021-04-20 08:40:33 +0800

    0415

--- 0415/index.js
+++ 0415/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './A1';
-
+import './index.css';
+import Clock from './Clock';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App brand='Ford'/>,
+  <React.StrictMode>
+    <Clock />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
