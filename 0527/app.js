0716bfa023c69cd125caf5e596007998c0b6f252
jw-zhen <C108156105@nkust.edu.tw>
2021-06-10 12:40:50 +0800

    0527

--- 0527/app.js
+++ 0527/app.js
@@ -16,7 +16,110 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 
 
-export default function SimpleMenu() {
+function ButtonAppBar() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(167),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+function Types() {
+  const useStyles = makeStyles({
+    root: {
+      width: '100%',
+      maxWidth: 500,
+    },
+  });
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography variant="h6" gutterBottom>
+        h6. Heading
+      </Typography>
+    </div>
+  );
+}
+
+function CustomCheckbox() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      color: theme.status.danger,
+      '&$checked': {
+        color: theme.status.danger,
+      },
+    },
+    checked: {},
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <Checkbox
+      defaultChecked
+      classes={{
+        root: classes.root,
+        checked: classes.checked,
+      }}
+    />
+  );
+}
+
+
+function App() {
+  
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
+  
+  const theme = createMuiTheme({
+    status: {
+      danger: orange[500],
+    },
+  });
+
+  const [checked, setChecked] = React.useState(true);
+
+  const [state, setState] = React.useState({
+    checkedA: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const classes1 = useStyles();
+
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = (event) => {
@@ -26,100 +129,59 @@ export default function SimpleMenu() {
   const handleClose = () => {
     setAnchorEl(null);
   };
-}
-const useStyles = makeStyles({
-  root: {
-      background:'linear-gradient(45deg, #333 ,#999)',
-      border: 0,
-      borderRadius:15,
-      color:'white',
-      padding:'5px 30px',
-      marginBottom:10,
-    },
-})
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main:green[400],
-    },secondary:{
-      main:orange[500],
-    }
-    },
-});
 
-function ButtonStyled(){
-  const classes = useStyles();
-return(
-  <Button className={classes.root}>
-    自帶樣式
-    </Button>
-)
-
-}
-
-function CheckboxExample(){
-  const [checked,setChecked] =React.useState(true);
-  return(
-    
-    <FormControlLabel>
-      control={<Checkbox 
-      checked={checked} 
-      onChange={(e)=>{setChecked(e.target.checked)}}  
-      inputProps={{'aria-label':'secomdary checkbox'}}
-      />}  
-      label="Checkbox 測試"
-    </FormControlLabel>
-    
-  )
-}
-
- function BasicTextFields() {
-  const classes = useStyles();
-
-  return (
-    <form className={classes.root} noValidate autoComplete="off">
-      
-    </form>
-  );
-}
-
-function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <theme>
-       <AppBar position="static">
-        <Toolbar>
-          <IconButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
+      <ThemeProvider theme={theme}>
+        
+        <ButtonAppBar />
+
+        <Types />
+        
+        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+          Open Menu
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
           <MenuItem onClick={handleClose}>Profile</MenuItem>
           <MenuItem onClick={handleClose}>My account</MenuItem>
           <MenuItem onClick={handleClose}>Logout</MenuItem>
         </Menu>
-          <Typography variant="h6" >
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-       <ButtonStyled/>
-       <TextField id="standard-basic" label="Standard" />
-      <TextField id="filled-basic" label="Filled" variant="filled" Type='date'/>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button startIcon={<SaveIcon />} size='large' onClick={() => { alert('clicked') }} variant="outlined" color="primary">
-        Primary
+
+        <form className={classes1.root} noValidate autoComplete="off">
+        <TextField
+          id="filled-secondary"
+          label="Filled secondary"
+          variant="filled"
+          color="secondary"
+        />
+        </form>
+        
+        <CustomCheckbox />
+
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={state.checkedA}
+              onChange={handleChange}
+              name="checkedA"
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+          }
+          label="Secondary"
+        />
+
+        <Button variant="contained" color="primary">
+          Primary
         </Button>
-        </theme>
+
+      </ThemeProvider>
       </header>
     </div>
   );
